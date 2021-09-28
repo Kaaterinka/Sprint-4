79ba767626717af8680db966cf71ac386b7a6422
Kaaterinka <katerinabolotina@inbox.ru>
2021-09-28 20:14:18 +0300

    Functional done

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,11 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> {
+        return students.filter { predicate(it) }
+    }
+
+    fun init(students: List<Student>) {
+        this.students = students
     }
 }
