2e5bd839ad6b89e01a88c452cc69a3a4c154cbfa
Kaaterinka <katerinabolotina@inbox.ru>
2021-10-03 13:31:52 +0300

    Generics done

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,55 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <T1, T2> compare(p1: Pair<T1, T2>, p2: Pair<T1, T2>): Boolean {
+    return p1 == p2
+}
 
-// 2.
+//// 2.
 fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+    var result = 0
+    for ((index, element) in anArray.withIndex()) {
+        if (element == elem)
+            result = index
+    }
+    return result
 }
 
-// 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+//// 3.
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        var tmp: T
+        for (i in list.lastIndex downTo 1) {
+            if (list[i] < list[i - 1]) {
+                tmp = list[i - 1]
+                list[i - 1] = list[i]
+                list[i] = tmp
+            }
+        }
+    }
 }
 
-// 4.
-class Stack {
 
+//// 4.
+class Stack<T> {
+    val stack = mutableListOf<T>()
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
+
+    fun push(value: T) {
+        stack.add(value)
+    }
+
+    fun pop(): T {
+        val value = stack.last()
+        if (!isEmpty())
+            stack.removeLast()
+        return value
+    }
 }
